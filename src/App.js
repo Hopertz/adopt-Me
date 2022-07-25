2c247930959d44285471963da66aef4bdfd62c9c
Hopertz <lugano.ngulwa@gmail.com>
2022-07-25 10:33:00 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { render } from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SearchParams from './SearchParams';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Adopt Me!</h1>
+            <SearchParams />
+        </div>
+    );
+};
+
+// const App = () => {
+//     return React.createElement('div', {}, [
+//         React.createElement('h1', {}, 'Adopt Me!'),
+//         React.createElement(Pet, {
+//             name: 'Luna',
+//             animal: 'Dog',
+//             breed: 'Havanese',
+//         }),
+
+//         React.createElement(Pet, {
+//             name: 'Pepper',
+//             animal: 'Bird',
+//             breed: 'Cockatiel',
+//         }),
+//         React.createElement(Pet, {
+//             name: 'Doink',
+//             animal: 'Cat',
+//             breed: 'Mix',
+//         }),
+//     ])
+// }
+
+render(<App />, document.getElementById('root'));
